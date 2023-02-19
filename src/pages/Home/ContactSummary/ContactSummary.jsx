33b78d1e3e00abbde6c9cca39589309a5fee5780
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-02-19 15:34:15 +0600

    contact summary res

--- src/pages/Home/ContactSummary/ContactSummary.jsx
+++ src/pages/Home/ContactSummary/ContactSummary.jsx
@@ -13,14 +13,14 @@ const ContactSummary = () => {
     // ];
 
     return (
-        <section className="h-[250px] bg-darkBlack rounded-lg flex flex-row items-center justify-center gap-14 my-[130px]">
+        <section className="md:h-[250px] h-auto bg-darkBlack rounded-lg flex md:flex-row flex-col md:items-center items-start px-7 md:px-0 justify-center md:gap-14 gap-5 md:my-[130px] my-[60px] py-7">
             <div className="text-white flex items-center justify-center gap-6">
                 <figure>
                     <img src={timetable} alt="timetable" />
                 </figure>
                 <div className="">
-                    <h4 className="font-medium text-sm mb-[6px]">We are open monday-friday</h4>
-                    <h4 className="text-[25px]">7:00 am - 9:00 pm</h4>
+                    <h4 className="font-medium md:text-sm text-[12px] mb-[6px]">We are open monday-friday</h4>
+                    <h4 className="md:text-[25px] text-lg">7:00 am - 9:00 pm</h4>
                 </div>
             </div>
             <div className="text-white flex items-center justify-center gap-6">
@@ -28,8 +28,8 @@ const ContactSummary = () => {
                     <img src={call} alt="timetable" />
                 </figure>
                 <div className="">
-                    <h4 className="font-medium text-sm mb-[6px]">Have a question?</h4>
-                    <h4 className="text-[25px]">+2546 251 2658</h4>
+                    <h4 className="font-medium md:text-sm text-[12px] mb-[6px]">Have a question?</h4>
+                    <h4 className="md:text-[25px] text-lg">+2546 251 2658</h4>
                 </div>
             </div>
             <div className="text-white flex items-center justify-center gap-6">
@@ -37,8 +37,8 @@ const ContactSummary = () => {
                     <img src={location} alt="timetable" />
                 </figure>
                 <div className="">
-                    <h4 className="font-medium text-sm mb-[6px]">Need a repair? our address</h4>
-                    <h4 className="text-[25px]">Liza Street, New York</h4>
+                    <h4 className="font-medium md:text-sm text-[12px] mb-[6px]">Need a repair? our address</h4>
+                    <h4 className="md:text-[25px] text-lg">Liza Street, New York</h4>
                 </div>
             </div>
         </section>
