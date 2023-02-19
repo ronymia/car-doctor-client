263461bb05ccc0e2798b928179bde5aa5d126be1
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-02-20 00:00:21 +0600

    core feature

--- src/pages/Home/CoreFeature/CoreFeature.jsx
+++ src/pages/Home/CoreFeature/CoreFeature.jsx
@@ -18,13 +18,13 @@ const CoreFeature = () => {
     }, []);
 
     return (
-        <section className="w-full flex flex-col justify-center items-center gap-6 my-[130px]">
-            <div className='text-center w-1/2 flex flex-col justify-center items-center'>
-                <p className="text-xl font-bold text-primary">Core Features</p>
-                <h2 className="text-[45px] font-semibold text-darkBlack">Why Choose Us</h2>
-                <p className="text-gray">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+        <section className="w-full flex flex-col justify-center items-center gap-6 md:my-[130px] my-[60px]">
+            <div className="text-center md:w-1/2 w-full flex flex-col justify-center items-center gap-1 md:gap-0">
+                <p className="md:text-xl text-lg font-bold text-primary">Core Features</p>
+                <h2 className="md:text-[45px] text-3xl font-semibold text-darkBlack">Why Choose Us</h2>
+                <p className="text-gray md:text-base text-sm">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
             </div>
-            <div className="grid grid-cols-6 gap-3">
+            <div className="grid md:grid-cols-6 grid-cols-2 gap-3">
                 {
                     coreFeature.map(feature => <CoreFeatureCard
                         key={feature._id}
