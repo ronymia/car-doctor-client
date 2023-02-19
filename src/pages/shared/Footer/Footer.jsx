74ed238a82d2e23d357db6dd865df31c75a179e6
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-02-20 00:14:00 +0600

    footer

--- src/pages/shared/Footer/Footer.jsx
+++ src/pages/shared/Footer/Footer.jsx
@@ -3,30 +3,30 @@ import logo from '../../../assets/logo.svg';
 
 const Footer = () => {
     return (
-        <footer className="footer p-32 bg-darkBlack text-white">
+        <footer className="md:p-32 p-8 grid md:grid-cols-4 grid-cols-2 bg-darkBlack text-white gap-5 items-center justify-center">
             <div>
                 <img src={logo} alt="" />
-                <p>Genius Car Doctor Industries Ltd.<br />Providing reliable tech since 1992</p>
+                <p className="md:text-base text-sm mt-2">Genius Car Doctor Ltd.<br />Providing reliable tech since 1992</p>
             </div>
-            <div>
+            <div className="flex flex-col items-start justify-center">
                 <span className="footer-title">Services</span>
-                <a href="/" className="link link-hover">Branding</a>
-                <a href="/" className="link link-hover">Design</a>
-                <a href="/" className="link link-hover">Marketing</a>
-                <a href="/" className="link link-hover">Advertisement</a>
+                <a href="/" className="link link-hover md:text-base text-sm">Branding</a>
+                <a href="/" className="link link-hover md:text-base text-sm">Design</a>
+                <a href="/" className="link link-hover md:text-base text-sm">Marketing</a>
+                <a href="/" className="link link-hover md:text-base text-sm">Advertisement</a>
             </div>
-            <div>
+            <div className="flex flex-col items-start justify-center">
                 <span className="footer-title">Company</span>
-                <a href="/" className="link link-hover">About us</a>
-                <a href="/" className="link link-hover">Contact</a>
-                <a href="/" className="link link-hover">Jobs</a>
-                <a href="/" className="link link-hover">Press kit</a>
+                <a href="/" className="link link-hover md:text-base text-sm">About us</a>
+                <a href="/" className="link link-hover md:text-base text-sm">Contact</a>
+                <a href="/" className="link link-hover md:text-base text-sm">Jobs</a>
+                <a href="/" className="link link-hover md:text-base text-sm">Press kit</a>
             </div>
-            <div>
+            <div className="flex flex-col items-start justify-center">
                 <span className="footer-title">Legal</span>
-                <a href="/" className="link link-hover">Terms of use</a>
-                <a href="/" className="link link-hover">Privacy policy</a>
-                <a href="/" className="link link-hover">Cookie policy</a>
+                <a href="/" className="link link-hover md:text-base text-sm">Terms of use</a>
+                <a href="/" className="link link-hover md:text-base text-sm">Privacy policy</a>
+                <a href="/" className="link link-hover md:text-base text-sm">Cookie policy</a>
             </div>
         </footer>
     );
