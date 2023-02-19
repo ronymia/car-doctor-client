4153261f8d3d60e351aed02abfc6998b227c5bc9
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-02-19 15:23:46 +0600

    about section mobile responsuve

--- src/pages/Home/About/About.jsx
+++ src/pages/Home/About/About.jsx
@@ -6,18 +6,18 @@ const About = () => {
     return (
         <div className="hero my-20">
             <div className="hero-content flex-col lg:flex-row">
-                <div className='relative w-1/2'>
-                    <img src={person} alt="" className="w-4/5 h-full rounded-lg shadow-2xl" />
+                <div className='relative md:w-1/2 w-4/5'>
+                    <img src={person} alt="" className="md:w-4/5 w-full md:h-full h-full rounded-lg shadow-2xl" />
                     <img src={parts} alt="" className="absolute right-5 top-1/2 w-3/5 border-8 rounded-lg shadow-2xl" />
                 </div>
-                <div className='w-1/2'>
+                <div className='lg:w-1/2 w-full mt-6 md:mt-0'>
                     <p className="text-xl font-bold text-primary">About Us</p>
-                    <h1 className="text-[45px] font-bold text-darkBlack">
+                    <h1 className="lg:text-[45px] text-2xl font-bold text-darkBlack">
                         We are qualified <br />
                         & of experience <br />
                         in this field</h1>
-                    <p className="py-6 text-gray">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
-                    <p className="pb-6 text-gray">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <p className="md:py-6 py-3 text-gray text-sm md:text-base">There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+                    <p className="md:py-6 pb-3 text-gray text-sm md:text-base">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
                     <button className="btn bg-primary text-white border-primary">Get More Info</button>
                 </div>
             </div>
