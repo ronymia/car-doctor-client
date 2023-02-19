18b91c918dbf229a93239c71a580b994ae23521e
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-02-20 00:03:44 +0600

    testimonial

--- src/pages/Home/Testimonial/Testimonial.jsx
+++ src/pages/Home/Testimonial/Testimonial.jsx
@@ -4,13 +4,13 @@ import TestimonialCard from './TestimonialCard';
 
 const Testimonial = () => {
     return (
-        <section className="w-full flex flex-col justify-center items-center gap-6 mb-[130px]">
-            <div className='text-center w-1/2 flex flex-col justify-center items-center'>
-                <p className="text-xl font-bold text-primary">Testimonial</p>
-                <h2 className="text-[45px] font-semibold text-darkBlack">What Customer Says</h2>
-                <p className="text-gray capitalize">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
+        <section className="w-full flex flex-col justify-center items-center gap-6 md:mb-[130px] mb-[60px]">
+            <div className="text-center md:w-1/2 w-full flex flex-col justify-center items-center md:gap-0 gap-1">
+                <p className="md:text-xl text-lg font-bold text-primary">Testimonial</p>
+                <h2 className="md:text-[45px] text-3xl font-semibold text-darkBlack">What Customer Says</h2>
+                <p className="text-gray capitalize md:text-base text-sm">the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable. </p>
             </div>
-            <section className="flex flex-row gap-8 relative">
+            <section className="flex md:flex-row flex-col gap-8 relative">
                 <TestimonialCard />
                 <div className="absolute top-1/2 left-[-2%] text-2xl h-[60px] w-[60px] bg-lightGray rounded-[50%] flex justify-center items-center text-darkBlack2 hover:bg-primary hover:text-white transition-all duration-500 cursor-pointer">
                     <AiOutlineArrowLeft />
