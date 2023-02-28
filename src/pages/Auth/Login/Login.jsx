908a2c1e0c644181b02b7afc679379c94e2f6444
MD Rony Mia <mdronymia.javascript@gmail.com>
2023-02-28 15:00:43 +0600

    emaiil password regex

--- src/pages/Auth/Login/Login.jsx
+++ src/pages/Auth/Login/Login.jsx
@@ -1,13 +1,88 @@
 import React from "react";
+import { Link } from "react-router-dom";
+import { useForm } from "react-hook-form";
+
+import loginImg from "../../../assets/images/login/login.svg";
 
 const Login = () => {
+  const { register, handleSubmit } = useForm();
+  const onSubmit = (data) => {
+    console.log(data);
+  };
+
   return (
-    <div>
+    <div className="flex flex-row items-center justify-center px-[150px] gap-10">
+      {/* login img  LEFT SIDE */}
       <div className="">
-        <img src="" alt="" />
+        <figure className="w-96 h-auto">
+          <img
+            src={loginImg}
+            alt="login svg..."
+            className="block w-full object-cover bg-cover "
+          />
+        </figure>
       </div>
-      <div className="">
-        <h1>Login</h1>
+
+      {/* login form  RIGHT SIDE*/}
+      <div className="h-[500px] rounded-[10px] border border-[#D0D0D0] flex flex-col items-center p-[50px]">
+        <h1 className="mb-6 text-4xl font-semibold block text-center text-darkBlack2">
+          Login
+        </h1>
+        <div className="">
+          <form
+            onSubmit={handleSubmit(onSubmit)}
+            className="flex flex-col gap-5"
+          >
+            {/* email input  */}
+            <div className="flex flex-col gap-2">
+              <label
+                htmlFor="email"
+                className="block text-base font-semibold text-darkBlack2 capitalize"
+              >
+                email
+              </label>
+              <input
+                type="text"
+                placeholder="your email"
+                className="block h-14 w-[460px] bg-white rounded-[10px] border border-[#E8E8E8] py-2 px-4 placeholder:capitalize placeholder:text-[15px] placeholder:text-[#A2A2A2] text-darkBlack font-medium text-lg"
+                {...register("email", {
+                  pattern: /^\S+@\S+\.\S+$/,
+                })}
+              />
+            </div>
+
+            {/* password  input */}
+            <div className="flex flex-col gap-2">
+              <label
+                htmlFor="password"
+                className="block text-base font-semibold text-darkBlack2 capitalize"
+              >
+                Passwrod
+              </label>
+              <input
+                type="text"
+                placeholder="your password"
+                className="block h-14 w-[460px] bg-white rounded-[10px] border border-[#E8E8E8] py-2 px-4 placeholder:capitalize placeholder:text-[15px] placeholder:text-[#A2A2A2] text-darkBlack font-medium text-lg"
+                {...register("password", {
+                  pattern: /^(?=.*[0-9])(?=.*[!@#$%^&*])[a-zA-Z0-9!@#$%^&*]/,
+                  minLength: 6,
+                  maxLength: 14,
+                })}
+              />
+              <Link className="block text-right text-darkBlack text-sm font-medium capitalize pr-2 mt-1 hover:underline hover:text-primary cursor-pointer">
+                forgot password
+              </Link>
+            </div>
+
+            {/* login button  */}
+            <button
+              type="submit"
+              className="btn bg-primary text-white border-primary text-lg font-semibold capitalize"
+            >
+              login
+            </button>
+          </form>
+        </div>
       </div>
     </div>
   );
